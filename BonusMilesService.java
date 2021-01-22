4a5fb32b23b3702a705096f26d130ee4b75e674d
Regina Shakirova <regina.shak@yandex.ru>
2021-01-22 22:41:01 +0300

    init

---
+++ BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        cost = 7000;
+        int bonusMilesService = cost / 20;
+        System.out.println(bonusMilesService);
+        return bonusMilesService;
+    }
+}
